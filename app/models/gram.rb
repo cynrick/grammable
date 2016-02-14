02bcd90de22ebe7644bc4bd7a34447e93e2b90bf
Cynrick Chiu <cyn.2k5@outlook.com>
2016-02-14 14:12:17 -0800

    Add validation for gram pictures

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,6 +1,7 @@
 class Gram < ActiveRecord::Base
   belongs_to :user
   validates :message, presence: true, length: { minimum: 3 }
+  validates :picture, presence: true
 
   mount_uploader :picture, PictureUploader
 end
