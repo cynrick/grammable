c4fa7955ce01cc505d5b6d343017bd7d2a5d08d4
Cynrick Chiu <cyn.2k5@outlook.com>
2016-02-13 16:53:23 -0800

    Update grams#create action

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -14,7 +14,7 @@ class GramsController < ApplicationController
     if @gram.valid?
       redirect_to root_path
     else
-      render :new, status: :unprocessable_entity
+      return render :new, status: :unprocessable_entity
     end
   end
 
