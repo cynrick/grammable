b42a7a6e864ea3af1f6c15101c9bbec1155200b3
Cynrick Chiu <cyn.2k5@outlook.com>
2016-02-15 13:26:24 -0800

    Add dependent: :destroy to relationships

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,6 +1,6 @@
 class Gram < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :message, presence: true, length: { minimum: 3 }
   validates :picture, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :grams
-  has_many :comments
+  has_many :grams, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
