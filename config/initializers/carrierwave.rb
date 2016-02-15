6666a7e9d46446fc1095d1a3a3f90b54eca309fb
Cynrick Chiu <cyn.2k5@outlook.com>
2016-02-14 18:50:11 -0800

    Use different settings for carrierwave depending on the Rails env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,19 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider              => 'AWS',
-    :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
-    :aws_secret_access_key => ENV['AWS_SECRET_KEY']
-  }
+# config/initializers/carrierwave.rb
 
-  config.fog_directory = ENV['AWS_BUCKET']
+CarrierWave.configure do |config|
+  if Rails.env.test? || Rails.env.cucumber?
+    config.storage = :file
+  end
+  
+  if Rails.env.production?
+    config.storage = :fog
+
+    config.fog_credentials = {
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['AWS_SECRET_KEY']
+    }
+
+    config.fog_directory = ENV['AWS_BUCKET']
+  end
 end
